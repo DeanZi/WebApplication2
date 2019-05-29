38703ede4521d1d4033c3ddf4e253cfe746f8b1b
Simon Bitton <simon.bitton1@live.biu.ac.il>
2019-05-29 18:28:12 +0300

    fix lon lat read method

--- WebApplication2/Controllers/DefaultController.cs
+++ WebApplication2/Controllers/DefaultController.cs
@@ -40,12 +40,31 @@ namespace WebApplication2.Controllers
             NetworkStream ns = server.GetStream();
             cmds[0] = longitude;
             cmds[1] = latitude;
-            foreach (string cmd in cmds)
-            {
-                string tmpCmd = cmd + "\r\n";
-                ns.Write(Encoding.ASCII.GetBytes(tmpCmd), 0, tmpCmd.Length);
-                ns.Flush();
-            }
+           
+            //while (server.Connected)  //while the client is connected, we look for incoming messages
+            //{
+                foreach (string cmd in cmds)
+                {
+                    string tmpCmd = cmd + "\r\n";
+                    ns.Write(Encoding.ASCII.GetBytes(tmpCmd), 0, tmpCmd.Length);
+                }
+                byte[] msg = new byte[1024];     // byte array
+                ns.Read(msg, 0, msg.Length);   //the networkstream now reads what is being sent from the client
+                char[] charsToTrim = { ' ', '?' };
+                string phrase = Encoding.Default.GetString(msg).Trim(charsToTrim);
+                Console.WriteLine(phrase); //we print the filtered input from the client
+                string[] details = phrase.Split(',', '\n');
+            details[0]=details[0].Replace("/position/longitude-deg = '", "");
+            details[0] = details[0].Replace("' (double)\r", "");
+            details[1] = details[1].Replace("/> /position/latitude-deg = '", "");
+            details[1] = details[1].Replace("' (double)\r", "");
+
+
+            /*    if (details[0] != "" && details[1] != "")
+                    Update(Convert.ToDouble(details[0]), Convert.ToDouble(details[1]));//we take the interesting first two
+              */  //pieces of information which stands for Lat and Lon and update our fields accordinglly.
+                  //}
+
             ns.Close();
             server.Close();
         }
